c1b0f35c03ae31609ad07c0c5d8216fe50587f6f
teriyakichan <mp5sniper@gmail.com>
2017-06-25 16:39:42 +0900

    Tweetボタン

---
+++ tsumige/Assets/Scripts/TweetButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using naichilab;
+
+public class TweetButton : MonoBehaviour
+{
+	public void Tweet()
+	{
+		decimal score = 0;
+		score = GameObject.Find("Script").GetComponent<ClickerController>().player.score;
+		UnityRoomTweet.Tweet(
+			"game-tsumutsumu",
+			"ゲーム積む積むで" + score.ToString("#,0").Split('.')[0] + "本ゲームを積みました",
+			"unity1week");
+	}
+}
